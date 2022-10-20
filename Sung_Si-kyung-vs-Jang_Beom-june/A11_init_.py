353bd83e96b1179c2086d042666a395e436aea7f
joosung-min <joosung_min@outlook.com>
2022-10-19 20:25:37 -0700

    B10 edits

--- Sung_Si-kyung-vs-Jang_Beom-june/A11_init_.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.chdir("/content/drive/MyDrive/personal/Sentiment-Analyses/Sung_Si-kyung-vs-Jang_Beom-june/")
-
-clova_id = "dgf7iav5gn"
-
-clova_secret = "1pTSu6qoFJ1tctVGLRA5oPXUo8j39GbALRFcAYhF"
-
-clova_api_url = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze"
